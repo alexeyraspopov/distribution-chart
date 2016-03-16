278939fed660c29b235fbfc2b45c5eeb94246a35
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-03-16 12:52:37 +0200

    added entry point

---
+++ index.web.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactPerf from 'react/lib/ReactDefaultPerf';
+
+ReactDOM.render((
+	<article>
+		
+	</article>
+), document.querySelector('main'));
+
+window.Perf = ReactPerf;
+window.React = React;
