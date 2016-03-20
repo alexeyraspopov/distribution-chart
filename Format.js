4d95cbcfdc76ea93a27a3c548454419a46acb56c
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-03-20 12:56:18 +0200

    added helpers

---
+++ Format.js
@@ -0,0 +1,5 @@
+import Format from 'd3-format';
+
+export const percentage = Format.format('.0%');
+export const precisePercentage = Format.format('.1%');
+export const fixedPoint = Format.format('.3f');
