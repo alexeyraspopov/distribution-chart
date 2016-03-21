904fdb0e5885f32d02b5b1e83619e3e844dd3854
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-03-21 13:04:11 +0200

    added <MotionPath />
    Transition component for SVG path

---
+++ MotionPath.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Motion, spring} from 'react-motion';
+import Interpolate from 'd3-interpolate';
+
+export default class MotionPath extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			interpolate: () => this.props.path,
+			direction: 1,
+			style: {x: 0},
+		};
+		this.interpolateChild = (value) => this.props.children(this.state.interpolate(value.x));
+	}
+
+	componentWillReceiveProps(nextProps) {
+		let interpolate;
+
+		if (!this.shouldComponentUpdate(nextProps)) {
+			return;
+		}
+
+		if (this.state.direction === 1) {
+			interpolate = Interpolate.interpolateString(this.props.path, nextProps.path);
+		} else {
+			interpolate = Interpolate.interpolateString(nextProps.path, this.props.path);
+		}
+
+		this.setState({
+			interpolate,
+			direction: 1 - this.state.direction,
+			style: {x: spring(this.state.direction)},
+		});
+	}
+
+	shouldComponentUpdate(nextProps) {
+		return this.props.path !== nextProps.path;
+	}
+
+	render() {
+		return <Motion style={this.state.style} children={this.interpolateChild} />;
+	}
+}
