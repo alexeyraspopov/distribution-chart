664ea20616ace64ea2221327f4f645fe29b58813
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-03-21 13:07:14 +0200

    added <DistributionContainer /> to the UI

--- index.web.js
+++ index.web.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactPerf from 'react/lib/ReactDefaultPerf';
+import DistributionContainer from 'DistributionContainer';
 import PanelContainer from 'PanelContainer';
 import InfoContainer from 'InfoContainer';
 
 ReactDOM.render((
 	<article className="row">
+		<section className="column">
+			<DistributionContainer />
+		</section>
+		<section className="column justified">
 			<InfoContainer />
 			<PanelContainer />
+		</section>
 	</article>
 ), document.querySelector('main'));
 
