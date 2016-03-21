17a0473512e961c496854b97b943dbe9dba081fc
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-03-21 12:59:36 +0200

    added <InfoContainer />

---
+++ InfoContainer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Container from 'flux/lib/fluxContainer';
+import Info from 'Info';
+import ObservationsStore from 'ObservationsStore';
+import PredictionStore from 'PredictionStore';
+
+class InfoContainer extends React.Component {
+	static getStores() {
+		return [ObservationsStore, PredictionStore];
+	}
+
+	static calculateState() {
+		return {
+			observations: ObservationsStore.at('observations'),
+			prediction: PredictionStore.getState(),
+		};
+	}
+
+	render() {
+		const {prediction, observations} = this.state;
+
+		return (
+			<Info prediction={prediction}
+				average={observations[Math.floor(observations.length * 0.5)]}
+				total={observations.length} />
+		);
+	}
+}
+
+export default Container.create(InfoContainer);
