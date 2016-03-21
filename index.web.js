1a76bca05fb705fba7ac27225b8857fe5afdd2b0
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-03-21 12:59:54 +0200

    added <InfoContainer /> to the UI

--- index.web.js
+++ index.web.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactPerf from 'react/lib/ReactDefaultPerf';
 import PanelContainer from 'PanelContainer';
+import InfoContainer from 'InfoContainer';
 
 ReactDOM.render((
 	<article className="row">
+			<InfoContainer />
 			<PanelContainer />
 	</article>
 ), document.querySelector('main'));
