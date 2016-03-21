374f71537531054c1e0ab9ca77c943bd4b9a2d62
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-03-21 12:57:15 +0200

    added PredictionStore
    Random prediction generator

---
+++ PredictionStore.js
@@ -0,0 +1,22 @@
+import ReduceStore from 'flux/lib/FluxReduceStore';
+import Dispatcher from 'Dispatcher';
+import Random from 'd3-random';
+
+const predictionGenerator = Random.randomNormal(0.5, 0.15);
+
+class PredictionStore extends ReduceStore {
+	getInitialState() {
+		return predictionGenerator();
+	}
+
+	reduce(state, action) {
+		switch (action.type) {
+		case 'PREDICTION_CHANGED':
+			return predictionGenerator();
+		default:
+			return state;
+		}
+	}
+}
+
+export default new PredictionStore(Dispatcher);
