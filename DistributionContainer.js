09f244e59424782e92f79c778e197ec4756b4369
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-03-21 13:06:55 +0200

    ...and container for it

---
+++ DistributionContainer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Container from 'flux/lib/fluxContainer';
+import Distribution from 'Distribution';
+import ObservationsStore from 'ObservationsStore';
+import Scale from 'd3-scale';
+import D3Array from 'd3-array';
+
+class DistributionContainer extends React.Component {
+	static getStores() {
+		return [ObservationsStore];
+	}
+
+	static calculateState() {
+		return {
+			observations: ObservationsStore.at('observations'),
+		};
+	}
+
+	render() {
+		return <Distribution
+			observations={this.state.observations} />;
+	}
+}
+
+export default Container.create(DistributionContainer);
