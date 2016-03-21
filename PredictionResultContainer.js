50be386de6b25145a567016e45f62e9954161d46
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-03-21 13:05:17 +0200

    added <PredicitonResultContainer />

---
+++ PredictionResultContainer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Container from 'flux/lib/fluxContainer';
+import PredictionResult from 'PredictionResult';
+import PredictionStore from 'PredictionStore';
+
+class PredictionResultContainer extends React.Component {
+	static getStores() {
+		return [PredictionStore];
+	}
+
+	static calculateState(xScale, yScale) {
+		return {
+			prediction: PredictionStore.getState(),
+		}
+	}
+
+	render() {
+		return <PredictionResult
+			prediction={this.state.prediction}
+			xScale={this.props.xScale}
+			yScale={this.props.yScale} />;
+	}
+}
+
+export default Container.create(PredictionResultContainer, {withProps: true});
